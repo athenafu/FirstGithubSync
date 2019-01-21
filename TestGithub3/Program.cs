98285b38ee11086423edc7aa3d65b01969e9b838
athenafu <fuqian3218@sina.com>
2019-01-21 15:06:08 +0800

    5

--- TestGithub3/Program.cs
+++ TestGithub3/Program.cs
@@ -11,6 +11,7 @@ namespace TestGithub3
         static void Main(string[] args)
         {
             Console.WriteLine("test Github!");
+            Console.WriteLine("default brand master");
         }
     }
 }
